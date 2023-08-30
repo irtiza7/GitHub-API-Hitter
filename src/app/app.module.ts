a41f7acd028390bb1bf666d0644a435d86b778e2
Muhammad Irtiza <muhammad.irtiza@rolustech.com>
2023-08-30 16:34:50 +0500

    Refactored code.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchUserComponent } from './Components/search-user/search-user.component';
-import { HttpClientModule } from '@angular/common/http';
 import { UserDetailsComponent } from './Components/user-details/user-details.component';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 import { NavbarComponent } from './Components/navbar/navbar.component';
 import { SearchResultsComponent } from './Components/search-results/search-results.component';
 
